ffdd0f418ce87f6b547ae6fc53ddeb31a767a420
LEIA\axelr <y.touge.rec@gmail.com>
2023-09-30 00:50:06 +0900

    マス目が表示されないことがある不具合の修正、ＣＰＵ戦の実装

--- WebSite/tic-tac-toe/scripts/play.js
+++ WebSite/tic-tac-toe/scripts/play.js
@@ -1,56 +1,108 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const gameBoard = document.querySelector(".game-board");
-    let currentPlayer = "○";
-    let moves = 0;
-    const cells = [];
+// 現在のプレイヤーを表す変数（'○' または '×'）
+let currentPlayer = '○';
 
-    const winningMessage = () => `${currentPlayer} の勝ち！`;
-    const drawMessage = () => `引き分け！`;
+// ゲームの状態を示す変数
+let gameActive = true;
 
-    for (let i = 0; i < 9; i++) {
-        const cell = document.createElement("div");
-        cell.addEventListener("click", cellClicked);
-        gameBoard.appendChild(cell);
-        cells.push(cell);
+// 終了フラグ
+let gameEnd = false;
+
+// ゲームボードの状態を保存する配列
+let gameState = ['', '', '', '', '', '', '', '', ''];
+
+// CPUとの対戦を示すフラグ
+let vsCPU = true;
+
+// 勝者のメッセージを返す関数
+const winningMessage = () => `${currentPlayer} の勝ち！`;
+
+// 引き分けのメッセージを返す関数
+const drawMessage = () => `引き分け！`;
+
+// セルがクリックされたときの処理
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+
+    if (gameState[clickedCellIndex] !== '' || !gameActive) {
+        return;
     }
 
-    function cellClicked(e) {
-        if (e.target.textContent === "") {
-            e.target.textContent = currentPlayer;
-            moves++;
-            if (checkWin()) {
-                setTimeout(() => {
-                    alert(winningMessage());
-                    resetGame();
-                }, 100);
-            } else if (moves === 9) {
-                setTimeout(() => {
-                    alert(drawMessage());
-                    resetGame();
-                }, 100);
-            } else {
-                currentPlayer = currentPlayer === "○" ? "×" : "○";
-            }
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+    
+    if (vsCPU && currentPlayer === '○' && gameActive) {
+        handleResultValidation();
+        if(!gameEnd){
+            cpuPlay();
+        }
+    } else {
+        handleResultValidation();
+    }
+}
+
+// ゲームの結果を確認する関数
+function handleResultValidation() {
+    const winningConditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],  // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],  // Columns
+        [0, 4, 8], [2, 4, 6]              // Diagonals
+    ];
+
+    let roundWon = false;
+    for (let i = 0; i < winningConditions.length; i++) {
+        const [a, b, c] = winningConditions[i];
+        if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
+            roundWon = true;
+            break;
         }
     }
 
-    function checkWin() {
-        const winningCombos = [
-            [0, 1, 2], [3, 4, 5], [6, 7, 8],
-            [0, 3, 6], [1, 4, 7], [2, 5, 8],
-            [0, 4, 8], [2, 4, 6]
-        ];
-        for (let combo of winningCombos) {
-            if (cells[combo[0]].textContent && cells[combo[0]].textContent === cells[combo[1]].textContent && cells[combo[0]].textContent === cells[combo[2]].textContent) {
-                return true;
-            }
-        }
-        return false;
+    if (roundWon) {
+        gameEnd = true;
+        setTimeout(() => {
+            alert(winningMessage());
+            resetGame();
+        }, 100);
+        return;
     }
 
-    window.resetGame = function() {
-        currentPlayer = "○";
-        moves = 0;
-        cells.forEach(cell => cell.textContent = "");
+    let roundDraw = !gameState.includes("");
+    if (roundDraw) {
+        gameEnd = true;
+        setTimeout(() => {
+            alert(drawMessage());
+            resetGame();
+        }, 100);
+        return;
     }
-});
+
+    currentPlayer = currentPlayer === '○' ? '×' : '○';
+}
+
+// CPUのプレイを制御する関数
+function cpuPlay() {
+    const availableCells = gameState.map((cell, index) => cell === "" ? index : null).filter(index => index !== null);
+    const randomCellIndex = availableCells[Math.floor(Math.random() * availableCells.length)];
+    const clickedCell = document.querySelectorAll('.cell')[randomCellIndex];
+
+    gameState[randomCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+
+    handleResultValidation();
+}
+
+// ゲームをリセットする関数
+function resetGame() {
+    gameState = ['', '', '', '', '', '', '', '', ''];
+    gameActive = true;
+    gameEnd = false;
+    currentPlayer = '○';
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = '');
+}
+
+// 各セルのクリックイベントリスナーを追加
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+
+// リセットボタンのクリックイベントリスナーを追加
+document.querySelector('button').addEventListener('click', resetGame);
